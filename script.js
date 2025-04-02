322a6a4a2cc0fb9599793fbe75cfe26c7467b3c2
Dmitry <234518@mail.ru>
2025-04-02 14:50:15 +0300

    Initial commit

---
+++ script.js
@@ -0,0 +1,74 @@
+let calendar = createCalendar();
+document.body.append(calendar);
+
+function createCalendar()
+{
+
+    let calendar = document.createElement("div");
+    calendar.className = "calendar";
+
+    let months = ["Январь","Февраль","Март","Апрель","Май","Июнь","Июль","Август","Сентябрь","Октябрь","Ноябрь","Декабрь"];
+    let daysOfWeek = ["Пн","Вт","Ср","Чт","Пт","Сб","Вс"];
+    let currentDate = new Date();
+    
+    let date = new Date();
+    for(let i=0; i<12; i++)
+    {
+        let card = document.createElement("div");
+        card.className = "card"
+        let title = document.createElement("h3");
+        title.innerText = months[i];
+        card.append(title);
+        
+        let table = document.createElement("table");
+        let header = document.createElement("tr");
+        for(let day of daysOfWeek)
+        {
+            let th = document.createElement("th");
+            th.innerText = day;
+            header.append(th);
+        }
+        table.append(header);
+
+
+        date.setMonth(i);
+        date.setDate(1);
+        let offset = date.getDay() - 1;
+        if(offset < 0)
+        {
+            offset += 7;
+        }
+        let lastRow = document.createElement("tr");
+        table.append(lastRow);
+
+        for(let j=0; j<offset; j++)
+        {
+            lastRow.append(document.createElement("td"));              
+        }
+
+        while(date.getMonth() == i)
+        {
+            if(lastRow.cells.length == 7)
+            {
+                lastRow = document.createElement("tr");
+                table.append(lastRow);
+            }
+            let td = document.createElement("td");
+            td.className = "day";
+
+            if(currentDate.getMonth() == date.getMonth() && currentDate.getDate() == date.getDate())
+            {
+                td.classList.add("current");
+            }
+
+            td.innerText = date.getDate();
+            lastRow.append(td);
+            date = new Date(date.getTime() + 3600*1000*24);               
+        }
+
+        card.append(table);
+        calendar.append(card);
+    }
+
+    return calendar;
+}
