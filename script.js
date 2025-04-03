6f4de0c76f6ff687100993d184ef3546c6dc72df
Dmitry <234518@mail.ru>
2025-04-03 15:57:26 +0300

    db

--- script.js
+++ script.js
@@ -1,23 +1,97 @@
 let calendar = createCalendar(showPopup);
 document.body.append(calendar);
 
+/**
+ * @type {IDBDatabase}
+ */
+let db;
+let request = indexedDB.open("calendar",1);
+request.onsuccess = e =>
+{
+    db = e.target.result;
+}
+request.onupgradeneeded = e =>
+{
+    let db = e.target.result;
+    let store = db.createObjectStore("events",{keyPath:"date"});
+    store.createIndex("hours","hours",{unique:false});
+    store.createIndex("minutes","minutes",{unique:false});
+    store.createIndex("description","description", {unique:false});
+}
+
+
+function addEvent(eventObject)
+{
+    return new Promise(resolve =>
+    {
+        if(!db)
+        {
+            resolve("no database");
+        }
+        else
+        {
+            let store = db.transaction("events", "readwrite").objectStore("events");
+            let request = store.add(eventObject);
+            request.onerror = e => resolve("error");
+            request.onsuccess = e => resolve("event saved");
+        }
+    })
+}
+
+
+function getEvents(dateString)
+{
+    return new Promise(resolve=>
+    {
+        if(!db)
+        {
+            resolve([]);
+        }
+        else
+        {
+            let store = db.transaction("events","readonly").objectStore("events");
+            let request = store.getAll(dateString);
+            request.onerror = e => resolve([]);
+            request.onsuccess = e => resolve(e.target.result);
+        }
+    })
+}
+
 
 function showPopup(date)
 {
+    let dateString = date.toLocaleDateString("ru");
+
     let popup = document.createElement("div");
     popup.className = "popup";
 
     let title = document.createElement("div");
     title.className = "title";
+    title.innerText = dateString;
 
-    title.innerText = date.toLocaleDateString("ru");
     let closeButton = document.createElement("button");
     closeButton.className = "close_button";
     closeButton.onclick = e => popup.remove();
-    popup.append(title,closeButton);
 
+    let table = document.createElement("table");
+    table.className = "eventsTable"
 
-    document.body.append(popup)
+    popup.append(title,closeButton,table);
+    document.body.append(popup);
+
+    getEvents(dateString).then(events =>     
+    {
+        for(let event of events)
+        {
+            let tr = document.createElement("tr");
+            let td1 = document.createElement("td");
+            td1.innerText = `${event.hours?(""+event.hours).padStart(2,"0"):"--"}:${event.minutes?(""+event.minutes).padStart(2,"0"):"--"}`;
+            let td2 = document.createElement("td");
+            td2.innerText = event.description? event.description : "";
+            tr.append(td1,td2);
+            table.append(tr);
+        }
+    })
 }
 
 function createCalendar(dateClicked)
